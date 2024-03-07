1e66dc3ec8d35af14f5a495a85b3717225d164d1
Pablo <maldonadomanzanopablo@gmail.com>
2024-03-06 18:21:35 -0600

    feat:change port

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -12,11 +12,11 @@ func Server(logger *zap.Logger) error {
 	r := chi.NewRouter()
 	r.Mount("/", Router(logger))
 
-	err := http.ListenAndServe(":3000", r)
+	err := http.ListenAndServe(":80", r)
 	if err != nil {
 		return err
 	} else {
-		fmt.Println("Server is running on PORT: 3000")
+		fmt.Println("Server is running on PORT: 80")
 	}
 
 	return nil
